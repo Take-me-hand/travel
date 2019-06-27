a7acf22d560f636516265030f77c85aac7a9787d
Take_me_hand <641343464@163.com>
2019-06-27 20:33:35 +0800

    project initialized

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+// 初始化样式文件
+import './assets/styles/reset.css'
+// 引入一像素问题解决文件
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 解决移动端300毫秒问题
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
