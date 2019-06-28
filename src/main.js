c0c10be154400e572f4549da7059953fb5bae139
Take_me_hand <641343464@163.com>
2019-06-28 20:39:39 +0800

    change

--- src/main.js
+++ src/main.js
@@ -4,16 +4,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-// 初始化样式文件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// 初始化样式样式表
 import 'styles/reset.css'
-// 引入一像素问题解决文件
+// 引入一像素问题解决的样式表
 import 'styles/border.css'
 // 引入字体图标样式表
 import 'styles/iconfont.css'
+// 引入轮播图插件的样式表
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 // 解决移动端300毫秒问题
 fastClick.attach(document.body)
+// 注册轮播图插件
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption" >
+			<!-- slides -->
+			<swiper-slide v-for="item of swiperLists" :key="item.id">
+				<img class="swiper-img" :src="item.imgUrl" alt="">
+			</swiper-slide>
+			<!-- Optional controls -->
+			<div class="swiper-pagination"  slot="pagination"></div>
+		</swiper>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomeSwiper',
+	data() {
+		return {
+			swiperOption: {
+				pagination: '.swiper-pagination',
+				loop:true
+			},
+			swiperLists:[
+				{id: '0001', imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/b421363bbbb9184deacb4247eeea7485.jpg_750x200_12b42643.jpg'},
+				{id: '0002', imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/818f6cc784ae6669b74bbbb255414a53.jpg_750x200_66ca5873.jpg'},
+				{id: '0003', imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/6ab65be3878a5ea365514cf3a8125fd1.jpg_750x200_7b2fe404.jpg'}
+				]
+   }
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	.wrapper >>> .swiper-pagination-bullet-active
+		background-color: #fff
+	.swiper-img
+		width: 100%
+</style>
